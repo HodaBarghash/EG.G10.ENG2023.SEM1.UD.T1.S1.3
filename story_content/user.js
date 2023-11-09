99f98fe0183f0e04f53dc0b6157b83f55302cda9
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-11-09 13:39:35 +0200

    g

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6YtXtqJFGAa":
+      case "6Tw2UZxrR7S":
         Script1();
         break;
   }
@@ -20,7 +20,7 @@ fetch(url,{
 	cache:'no-cache',
 	headers: {'Content-Type': 'application/json'},
 	redirect:'follow',
-	body: JSON.stringify({v21q1attempts:player.GetVar("v21q1attempts"),v21q1answeredcorrect:player.GetVar("v21q1answeredcorrect"),v21q2attempts:player.GetVar("v21q2attempts"),v21q2answeredcorrect:player.GetVar("v21q2answeredcorrect"),v10q3attempts:player.GetVar("v21q3attempts"),v21q3answeredcorrect:player.GetVar("v21q3answeredcorrect")})
+	body: JSON.stringify({v21q1attempts:player.GetVar("v21q1attempts"),v21q1answeredcorrect:player.GetVar("v21q1answeredcorrect"),v21q2attempts:player.GetVar("v21q2attempts"),v21q2answeredcorrect:player.GetVar("v21q2answeredcorrect"),v21q3attempts:player.GetVar("v21q3attempts"),v21q3answeredcorrect:player.GetVar("v21q3answeredcorrect")})
 	}
 	)
 }
